c96a35870e79959d8f42f44d0a9ae42194bd1a11
Hugo de Yrigoyen <hugo.deyrigoyen@gmail.com>
2024-09-27 09:27:11 +0200

    iteration

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,4 @@
-async function getList(el, subEl = null) {
-  let list = [];
-
+async function getList() {
   return fetch("../data/recipes.json")
     .then(function (res) {
       if (res.ok) {
@@ -8,24 +6,70 @@ async function getList(el, subEl = null) {
       }
     })
     .then(function (recipes) {
-      list = recipes.flatMap(recipe => {
-        const value = recipe[el];
-        
-        if (Array.isArray(value)) {
-          return subEl ? value.map(item => item[subEl]).filter(Boolean) : value;
-        }
-        
-        return [value];
-      })
+      let recipe = recipes;
 
-      return [...new Set(list)];
+      let ingredients = [
+        ...new Set(
+          recipes.flatMap((recipe) => {
+            return recipe["ingredients"].map((e) => e["ingredient"]);
+          })
+        ),
+      ];
+      let ustensils = [
+        ...new Set(
+          recipes.flatMap((recipe) => {
+            return recipe["ustensils"];
+          })
+        ),
+      ];
+      let appliances = [
+        ...new Set(
+          recipes.map((recipe) => {
+            return recipe["appliance"];
+          })
+        ),
+      ];
+
+      return {
+        recipe: recipe,
+        ingredients: ingredients,
+        ustensils: ustensils,
+        appliances: appliances,
+      };
     })
     .catch(function (err) {
       console.log(err);
     });
 }
 
-console.log(getList("name"));
-console.log(getList("ingredients", "ingredient"));
-console.log(getList("ustensils"));
-console.log(getList("appliance"));
+async function init() {
+  try {
+    const jsonInfos = await getList();
+    const recipe = jsonInfos.recipe;
+    const ingredients = jsonInfos.ingredients;
+    const ustensils = jsonInfos.ustensils;
+    const appliances = jsonInfos.appliances;
+
+    let ingredientsFiltered = "lait de coco"
+    let recipeFiltered = recipe.map((r) => {
+      r.ingredient.contains(ingredientsFiltered) //changer contains
+    })
+  } catch (error) {
+    console.log("Erreur :", error);
+  }
+}
+
+init();
+
+// let r = function getList2() {};
+// let u = function getUstensils(r) {};
+// let i = function getIngredients(r) {};
+// let a = function getAppareils(r) {};
+// // on rajoute un filtre
+// let rf = function getListFiltered(filter) {};
+// let uf = function getUstensils(rf) {};
+// let iff = function getIngredients(rf) {};
+// let af = function getAppareils(rf) {};
+
+// let ua = function getSelectedElements(id) {};
+// function generateList(ua, uf) {}
